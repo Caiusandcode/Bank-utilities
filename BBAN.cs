d01d85c37c6f797b73e6c640238456c0459e393e
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-03-01 19:26:28 +0200

    Add files via upload

---
+++ BBAN.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bank_bban
+{
+    class BBAN
+    {
+
+        public string Account { get; set; }
+        public bool IsMachineNumber { get; set; }
+        public string MachineNumber { get; set; }
+
+
+        public BBAN(string bban)
+        {
+            Account = bban;
+            string tmpRefAccount = Account;
+            IsMachineNumber = Checkprogram.CorrectNumber(ref tmpRefAccount);
+            MachineNumber = tmpRefAccount;
+        }
+
+        public bool IsValid()
+        {
+            return Checkprogram.IsValidAccount(Account);
+        }
+
+        public override string ToString()
+        {
+            return $"BBAN: {Account}\n" +
+                   $"MachineNumber: {MachineNumber}\n" +
+                   $"IsValid: {IsMachineNumber}\n" +
+                   $"--------------------------\n";
+        }
+    }
+}
---
+++ Program.Bban.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bank_bban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BBAN BBANnew = new BBAN("227720A35988");
+            string machineFormat = BBANnew;
+            BBANnew.IsMachineNumber = Checkprogram.CorrectNumber(ref machineFormat);
+            BBANnew = machineFormat;
+            Console.WriteLine(BBANnew.ToString());
+       
+            BBAN account_2 = new BBAN("123456-781");
+            Console.WriteLine(account_2.ToString());
+
+
+            Console.WriteLine($"{BBANnew} - {BBANnew.IsValid()}");
+
+            string machineformat = "A227720-35988";
+            bool isMachineFormat = Checkprogram.CorrectNumber(ref machineFormat);
+            if (isMachineFormat)
+                Console.WriteLine($"{machineFormat} - {Checkprogram.IsValidAccount(machineFormat)}");
+            else
+                Console.WriteLine("Account number incorrect!");
+        }
+    }
+}
